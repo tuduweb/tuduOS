4f6413301a8d96ff0ece09bf94d25accb62d4a2c
bin <tuduweb@qq.com>
2020-08-22 12:56:27 +0800

    修改调试syscall，增加mailbox部分syscall调用。

--- components/lwp/lwp_syscall.c
+++ components/lwp/lwp_syscall.c
@@ -222,7 +222,8 @@ int sys_notimpl(void)
 
 void bin_syscall_test(int cnt)
 {
-    rt_kprintf("syscall[0x21] %d\n",cnt);
+    rt_thread_t tid = rt_thread_self();
+    rt_kprintf("%s call %d\n", tid->name, cnt);
 }
 
 
@@ -274,11 +275,47 @@ const static void* func_table[] =
 
 extern rt_thread_t sys_thread_create(const char *name, void (*entry)(void *parameter), void *parameter, rt_uint32_t stack_size, rt_uint8_t  priority, rt_uint32_t tick);
 extern rt_err_t sys_thread_startup(rt_thread_t thread);
+extern rt_err_t sys_thread_mdelay(rt_int32_t ms);
+
+extern rt_mutex_t rt_mutex_create(const char *name, rt_uint8_t flag);
+extern rt_err_t rt_mutex_delete(rt_mutex_t mutex);
+extern rt_err_t rt_mutex_take(rt_mutex_t mutex, rt_int32_t time);
+extern rt_err_t rt_mutex_release(rt_mutex_t mutex);
+
+extern rt_mailbox_t rt_mb_create(const char *name, rt_size_t size, rt_uint8_t flag);
+extern rt_err_t rt_mb_delete(rt_mailbox_t mb);
+extern rt_err_t rt_mb_send(rt_mailbox_t mb, rt_ubase_t value);
+extern rt_err_t rt_mb_recv(rt_mailbox_t mb, rt_ubase_t *value, rt_int32_t timeout);
+
+rt_mailbox_t sys_mb_create(const char *name, rt_size_t size, rt_uint8_t flag)
+{
+    return rt_mb_create(name, size, flag);
+}
+rt_err_t sys_mb_delete(rt_mailbox_t mb)
+{
+    return rt_mb_delete(mb);
+}
+rt_err_t sys_mb_send(rt_mailbox_t mb, rt_ubase_t value)
+{
+    return rt_mb_send(mb, value);
+}
+rt_err_t sys_mb_recv(rt_mailbox_t mb, rt_ubase_t *value, rt_int32_t timeout)
+{
+    return rt_mb_recv(mb, value, timeout);
+}
+
+
 //浮动一下,从0x60开始吧
 const static void* func_table2[] =
 {
     (void *)sys_thread_create,//0x60
     (void *)sys_thread_startup,//0x61
+    (void *)sys_thread_mdelay,//0x62
+
+    (void *)sys_mb_create,
+    (void *)rt_mb_delete,
+    (void *)rt_mb_send,
+    (void *)rt_mb_recv,
 };
 
 
@@ -310,3 +347,11 @@ const void *lwp_get_sys_api(rt_uint32_t number)
 
     return func;
 }
+
+rt_err_t sys_thread_mdelay(rt_int32_t ms)
+{
+    return rt_thread_mdelay(ms);
+}
+
+
+
