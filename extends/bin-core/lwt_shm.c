3d6a67c90c9924a4059f48923308e8f8e3deaf95
bin <tuduweb@qq.com>
2020-08-28 23:18:51 +0800

    bmem_malloc实现，手动测试正常。需要注意初始化时头节点的问题。

--- extends/bin-core/lwt_shm.c
+++ extends/bin-core/lwt_shm.c
@@ -1,6 +1,11 @@
 #include <rthw.h>
 #include <rtthread.h>
 #include "lwt_shm.h"
+
+#define DBG_TAG    "SHM"
+#define DBG_LVL    DBG_WARNING
+#include <rtdbg.h>
+
 /**
  * 共享内存
  */
@@ -8,14 +13,152 @@
 struct _lwt_shm lwt_shm;
 #define LWT_SHM_SIZE 0x4000
 
-rt_err_t bmem_create(void* start,rt_uint32_t size)
+
+struct bmem_block* get_mem_block(struct bmem_typedef* bmem)
 {
-    return RT_EOK;
+    struct bmem_tab* bmem_tab;
+    struct bmem_block* bmem_block;
+
+    int tab_num = 0;
+
+    while(1)
+    {
+
+        /* 遍历 tab_head,寻找空闲 */
+        /* bmem->tab_head是普通节点rt_list_t型,其他的为数据节点bmem_block型 */
+        for(bmem_tab = (struct bmem_tab*)bmem->tab_head.next; bmem_tab != (struct bmem_tab*)&bmem->tab_head; bmem_tab = (struct bmem_tab*)bmem_tab->node.next)
+        {
+            
+            if(bmem_tab->used_num < BLOCK_TAB_SIZE)
+            {
+                for(tab_num = 0; tab_num < BLOCK_TAB_SIZE; tab_num++)
+                {
+                    bmem_block = bmem_tab->tab + tab_num;
+
+                    //找到空闲的节点
+                    if(bmem_block->is_used == 0)
+                    {
+                        bmem_tab->used_num++;
+                        return bmem_block;//记住需要初始化
+                    }
+                }
+            }
+        }
+
+        //如果没有空闲的tab_head,那么申请新的
+        bmem_tab = rt_calloc(1, sizeof(struct bmem_tab));// + sizeof(struct bmem_block)*BLOCK_TAB_SIZE
+        if(bmem_tab)
+        {
+            bmem_tab->node.prev = &bmem_tab->node;
+            bmem_tab->node.next = &bmem_tab->node;
+            bmem_tab->used_num = 0;
+            rt_list_insert_after(&bmem->tab_head, &bmem_tab->node);
+        }else{
+            return RT_NULL;
+        }
+
+    }
+
+    //return RT_NULL;
 }
 
-void *bmem_alloc(rt_uint32_t size)
+struct bmem_typedef* bmem_create(rt_uint32_t start, rt_uint32_t size)
 {
-    return 0;
+    struct bmem_typedef* bmem;
+    struct bmem_block* bmem_block;
+
+    //TODO:字节对齐处理
+    bmem = rt_calloc(1, sizeof(struct bmem_typedef));
+
+    if(bmem)
+    {
+        bmem->start = (char *)start;
+        bmem->mem_size = size;
+
+        bmem->block_node.prev = &bmem->block_node;
+        bmem->block_node.next = &bmem->block_node;
+
+        bmem->tab_head.prev = &bmem->tab_head;
+        bmem->tab_head.next = &bmem->tab_head;
+
+        bmem_block = rt_calloc(1, sizeof(struct bmem_block));//get_mem_block(bmem);//需要解决头个被重复返回的问题
+        if(bmem_block)
+        {
+            bmem_block->len = bmem->mem_size;
+            bmem_block->is_used = 0;
+            bmem_block->node.prev = &bmem_block->node;
+            bmem_block->node.next = &bmem_block->node;
+
+            rt_list_insert_after(&bmem->block_node, &bmem_block->node);
+        }else{
+            rt_free(bmem);
+            bmem = RT_NULL;
+        }
+    }
+
+    return bmem;
+}
+
+/**
+ * @name 申请bmem
+ * @brief 在create里面的mem_block中,分出更多的block
+ */
+void *bmem_malloc(struct bmem_typedef* bmem, rt_uint32_t size)
+{
+    struct bmem_block* bmem_block;
+    struct bmem_block* new_bmem_block;
+    rt_uint32_t mem_offset = 0;
+
+    if(bmem->mem_size >= size)
+    {
+        for(bmem_block = (struct bmem_block*)bmem->block_node.next;
+            bmem_block != (struct bmem_block*)&bmem->block_node;
+            bmem_block = (struct bmem_block*)bmem_block->node.next)
+        {
+            if(bmem_block->is_used == 0)
+            {
+                //大小不符合要求
+                if(bmem_block->len >= size)
+                {
+
+                    if(bmem_block->len > size)
+                    {
+                        //拆分
+                        new_bmem_block = get_mem_block(bmem);
+                        if(!new_bmem_block)
+                            return RT_NULL;
+                        
+                        new_bmem_block->is_used = 1;
+                        new_bmem_block->len = size;
+                        new_bmem_block->node.next = &new_bmem_block->node;
+                        new_bmem_block->node.prev = &new_bmem_block->node;
+                        //插入,拆分
+                        bmem_block->len = bmem_block->len - size;//大小拆分
+                        //插入到空闲的前面
+                        rt_list_insert_before(&bmem_block->node, &new_bmem_block->node);
+                    }
+
+                    if(bmem_block->len == size)
+                    {
+                        //使用
+                        bmem_block->is_used = 1;
+                    }
+                    
+                    bmem->used_mem_size += size;//更新已经占用大小
+                    if(bmem->used_mem_size > bmem->used_max_mem_size)
+                        bmem->used_max_mem_size = bmem->used_mem_size;//更新最大占用
+                    
+                    return (void *)&(bmem->start[mem_offset]);
+
+                }
+
+            }
+            //偏移地址累加
+            mem_offset += bmem_block->len;
+        }
+    }
+
+    return RT_NULL;
 }
 
 rt_err_t bmem_free(void* addr)
@@ -24,12 +167,21 @@ rt_err_t bmem_free(void* addr)
 }
 
 
-rt_err_t lwt_shm_init()
+int lwt_shm_init()
 {
-    rt_base_t* shm = RT_KERNEL_MALLOC(LWT_SHM_SIZE);
+    rt_uint32_t shm = (rt_uint32_t)RT_KERNEL_MALLOC(LWT_SHM_SIZE);
 
     rt_memset(&lwt_shm, 0, sizeof(struct _lwt_shm));
-    lwt_shm.shm_addr = shm;
+
+    lwt_shm.bmem = bmem_create(shm, LWT_SHM_SIZE);
+
+    if(!lwt_shm.bmem)
+    {
+        LOG_E("shm Error");
+        return -RT_ERROR;
+    }
+
+    lwt_shm.shm_addr = (rt_uint32_t *)shm;
     lwt_shm.shm_size = LWT_SHM_SIZE;
 
     lwt_shm.app.prev = &lwt_shm.app;
@@ -39,6 +191,8 @@ rt_err_t lwt_shm_init()
     
     return RT_EOK;
 }
+
+INIT_ENV_EXPORT(lwt_shm_init);
 
 void *lwt_shm_alloc(int size)
 {
@@ -54,3 +208,12 @@ rt_err_t lwt_shm_free(void* addr)
 {
     return RT_EOK;
 }
+
+void malloc_test(int argc,char* argv[])
+{
+    rt_uint32_t addr = 0;
+    addr = (rt_uint32_t)bmem_malloc(lwt_shm.bmem, 100);
+    rt_kprintf("addr:%x\r\n", addr);
+}
+MSH_CMD_EXPORT(malloc_test,malloc!);
+
--- extends/bin-core/lwt_shm.h
+++ extends/bin-core/lwt_shm.h
@@ -3,18 +3,31 @@
 
 #include "rtdef.h"
 
-struct _bmem_type
-{
-    rt_uint32_t start;
-    rt_uint32_t size;
-    rt_list_t   block_node;
-};
-
 struct bmem_block
 {
+    rt_list_t       node;
+    rt_uint16_t     is_used;//block_is_used
+    rt_uint16_t     len;//block_size
+};
+
+#define BLOCK_TAB_SIZE 8
+
+struct bmem_tab
+{
     rt_list_t   node;
-    uint16_t    is_used;//block_is_used
-    uint16_t    size;//block_size
+    rt_uint32_t used_num;
+    struct bmem_block tab[BLOCK_TAB_SIZE];
+};
+
+struct bmem_typedef
+{
+    char*       start;
+    rt_uint32_t mem_size;
+    rt_uint32_t used_mem_size;
+    rt_uint32_t used_max_mem_size;
+    
+    rt_list_t   tab_head;
+    rt_list_t   block_node;
 };
 
 struct shm_mem
@@ -49,8 +62,10 @@ struct shm_relation
 
 struct _lwt_shm
 {
+    struct bmem_typedef* bmem;
     rt_uint32_t* shm_addr;
     rt_uint32_t shm_size;
+
 
     struct shm_relation *relation_tab;
     struct shm_app      *app_tab;
